6b756add7703f97eb58fffd1a6391ee4208691ba
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2024-02-15 16:28:21 +0330

    feat: implement restore  method for deleted discounts in admin panel

--- app/Filament/Admin/Resources/DiscountResource.php
+++ app/Filament/Admin/Resources/DiscountResource.php
@@ -88,6 +88,7 @@ class DiscountResource extends Resource
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
                 Tables\Actions\ForceDeleteAction::make(),
+                Tables\Actions\RestoreAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
